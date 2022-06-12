deaf3e8d317e912e7b2ee3e1fb1b262b3fac7454
Andrew <andrew.am2017@gmail.com>
2022-06-12 20:28:02 +0300

    создал таблицу Product и первую миграцию

--- formula_podarka/formula/models.py
+++ formula_podarka/formula/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Product(models.Model):
+    article = models.IntegerField()
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+
