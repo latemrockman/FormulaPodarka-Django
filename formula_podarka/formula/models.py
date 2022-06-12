bc0eee1bf3367f4f7281dda865ace903f54d9c71
Andrew <andrew.am2017@gmail.com>
2022-06-12 21:14:20 +0300

    создал связь товаров и категрий

--- formula_podarka/formula/models.py
+++ formula_podarka/formula/models.py
@@ -2,24 +2,34 @@ from django.db import models
 
 # Create your models here.
 
-
-
-class Product(models.Model):
-    article = models.IntegerField()
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.FloatField()
-
-    class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
-
 class Category(models.Model):
-    name = models.ForeignKey(Product, on_delete=models.PROTECT, null=True)
+    name = models.CharField(default='', max_length=50)
 
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+
+class Product(models.Model):
+    article = models.IntegerField()
+    title = models.CharField(default='', max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
 
 
