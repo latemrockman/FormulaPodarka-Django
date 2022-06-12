682f14d60d15756e9e6da4ce24580dae1e746d5a
Andrew <andrew.am2017@gmail.com>
2022-06-12 18:37:24 +0300

    составил роуты на все страницы

--- formula_podarka/formula/urls.py
+++ formula_podarka/formula/urls.py
@@ -4,4 +4,31 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home-page'),
+    path('catalog/', views.catalog, name='catalog-page'),
+
+    path('company/', views.company, name='company-page'),
+    path('company/awards/', views.awards, name='awards-page'),
+    path('company/technologies/', views.technologies, name='technologies-page'),
+
+    path('clients/', views.clients, name='clients-page'),
+    path('clients/payment/', views.payment, name='payment-page'),
+    path('clients/delivery/', views.delivery, name='delivery-page'),
+
+    path('clients/contacts/', views.contacts, name='contacts-page'),
 ]
+
+
+
+
+
+"""
+home
+catalog
+company
+- awards
+- technologies
+clients
+- payment
+- delivery
+contacts
+"""
--- formula_podarka/formula/views.py
+++ formula_podarka/formula/views.py
@@ -6,3 +6,44 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'formula/index.html', {})
+
+def catalog(request):
+    pass
+
+def company(request):
+    pass
+
+def awards(request):
+    pass
+
+def technologies(request):
+    pass
+
+def clients(request):
+    pass
+
+def payment(request):
+    pass
+
+def delivery(request):
+    pass
+
+def contacts(request):
+    pass
+
+
+
+
+
+
+"""
+home
+catalog
+company
+- awards
+- technologies
+clients
+- payment
+- delivery
+contacts
+"""
