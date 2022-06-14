7ede9aab37ff684d80e972cd2a40fd10936abf1c
Andrew <andrew.am2017@gmail.com>
2022-06-14 19:25:30 +0300

    Подключил шаблон

--- formula_podarka/formula/urls.py
+++ formula_podarka/formula/urls.py
@@ -9,18 +9,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home-page'),
+
     path('catalog/', views.catalog, name='catalog-page'),
-
-
-
     path('catalog/<slug:category_slug>', views.category, name='category-page'),
     path('catalog/<slug:category_slug>/<slug:product_slug>/', views.product, name='product-page'),
-
-
-
-
-
-
 
     path('company/', views.company, name='company-page'),
     path('company/awards/', views.awards, name='awards-page'),
--- formula_podarka/formula/views.py
+++ formula_podarka/formula/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 from .models import Category, Product
 
-# Create your views here.
 
+# Create your views here.
 
 
 def index(request):
     categories = Category.objects.order_by('-id')
     products = Product.objects.order_by('-id')
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
     return render(request, 'formula/index.html', context)
+
 
 def catalog(request):
     categories = Category.objects.order_by('-id')
     products = Product.objects.order_by('-id')
 
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
 
     return render(request, 'formula/catalog.html', context)
@@ -34,17 +35,16 @@ def category(request, category_slug):
     category_name = Category.objects.get(slug=category_slug).name.lower().capitalize()
     products_of_category = Product.objects.filter(category_id=category_id)
 
-
-
     context = {
-        'categories' : categories,
-        'products'   : products,
+        'categories': categories,
+        'products': products,
         'category_slug': category_slug,
         'products_of_category': products_of_category,
         'category_id': category_id,
         'category_name': category_name
     }
     return render(request, 'formula/category.html', context)
+
 
 def product(request, category_slug, product_slug):
     categories = Category.objects.order_by('-id')
@@ -56,10 +56,9 @@ def product(request, category_slug, product_slug):
     product_description = Product.objects.get(slug=product_slug).description
     product_price = Product.objects.get(slug=product_slug).price
 
-
     context = {
-        'categories' : categories,
-        'products'   : products,
+        'categories': categories,
+        'products': products,
         'category_slug': category_slug,
         'product_slug': product_slug,
         'category_name': category_name,
@@ -69,8 +68,6 @@ def product(request, category_slug, product_slug):
         'product_price': product_price
     }
 
-
-
     return render(request, 'formula/product.html', context)
 
 
@@ -79,74 +76,76 @@ def company(request):
     products = Product.objects.order_by('-id')
 
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
     return render(request, 'formula/company.html', context)
+
 
 def awards(request):
     categories = Category.objects.order_by('-id')
     products = Product.objects.order_by('-id')
 
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
     return render(request, 'formula/awards.html', context)
+
 
 def technologies(request):
     categories = Category.objects.order_by('-id')
     products = Product.objects.order_by('-id')
 
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
     return render(request, 'formula/technologies.html', context)
+
 
 def clients(request):
     categories = Category.objects.order_by('-id')
     products = Product.objects.order_by('-id')
 
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
     return render(request, 'formula/clients.html', context)
+
 
 def payment(request):
     categories = Category.objects.order_by('-id')
     products = Product.objects.order_by('-id')
 
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
     return render(request, 'formula/payment.html', context)
+
 
 def delivery(request):
     categories = Category.objects.order_by('-id')
     products = Product.objects.order_by('-id')
 
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
     return render(request, 'formula/delivery.html', context)
+
 
 def contacts(request):
     categories = Category.objects.order_by('-id')
     products = Product.objects.order_by('-id')
 
     context = {
-        'categories' : categories,
-        'products'   : products
+        'categories': categories,
+        'products': products
     }
     return render(request, 'formula/contacts.html', context)
-
-
-
-
 
 
 """
